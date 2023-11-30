9c6cca040c5cbf374e11f4864343a42261cdb353
koteswararao798988 <109726258+koteswararao798988@users.noreply.github.com>
2023-11-30 16:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello APPS DEPLOYER!\n'
-              'Hello Node!\n'
   res.end(msg);
 });
 
